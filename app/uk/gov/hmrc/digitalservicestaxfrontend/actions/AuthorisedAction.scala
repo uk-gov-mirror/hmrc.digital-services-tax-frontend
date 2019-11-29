44785e720958bc19999dce0170ee8c3e59dca811
adamdye-hmrc <adam.dye@digital.hmrc.gov.uk>
2019-11-29 15:01:51 +0000

    fixed merge conflicts.

--- app/uk/gov/hmrc/digitalservicestaxfrontend/actions/AuthorisedAction.scala
+++ app/uk/gov/hmrc/digitalservicestaxfrontend/actions/AuthorisedAction.scala
@@ -14,22 +14,6 @@
  * limitations under the License.
  */
 
-///*
-// * Copyright 2019 HM Revenue & Customs
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *     http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
-//
 //package uk.gov.hmrc.digitalservicestaxfrontend.actions
 //
 //import javax.inject.Inject
@@ -45,7 +29,9 @@
 //import uk.gov.hmrc.play.HeaderCarrierConverter
 //
 //import scala.concurrent.{ExecutionContext, Future}
-//
+
+//TODO implement auth
+
 //class AuthorisedAction @Inject()(mcc: MessagesControllerComponents, val authConnector: AuthConnector)
 //  (implicit val appConfig: AppConfig, val executionContext: ExecutionContext)
 //  extends ActionBuilder[AuthorisedRequest, AnyContent] with ActionRefiner[Request, AuthorisedRequest] with AuthorisedFunctions {
