31122719bd9ea66332a04d1df624996026840d31
adamdye-hmrc <adam.dye@digital.hmrc.gov.uk>
2020-02-28 14:52:37 +0000

    corrected key.

--- app/uk/gov/hmrc/digitalservicestax/journeys/RegJourney.scala
+++ app/uk/gov/hmrc/digitalservicestax/journeys/RegJourney.scala
@@ -48,7 +48,7 @@ object RegJourney {
         // found a matching company
         case Some(company) =>
           for {
-            confirmCompany <- interact[Company, Boolean]("confirm-company", company)
+            confirmCompany <- interact[Company, Boolean]("confirm-company-to-register", company)
             //TODO Check if we should be signing the user out
             _ <- if (!confirmCompany) { tell("details-not-correct", Kickout("details-not-correct")) } else { (()).pure[F] }
           } yield (company)
