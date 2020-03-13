a88f4d46c92ee0aac61fae83832c2cd3e179bc1c
Luke Tebbs <luke.tebbs@digital.hmrc.gov.uk>
2020-03-13 15:01:05 +0000

    update HMRC dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,22 +9,21 @@ object AppDependencies {
 
   val compile = Seq(
 
-    "uk.gov.hmrc"             %% "govuk-template"           % "5.44.0-play-26",
-    "uk.gov.hmrc"             %% "play-ui"                  % "8.3.0-play-26",
+    "uk.gov.hmrc"             %% "govuk-template"           % "5.52.0-play-26",
+    "uk.gov.hmrc"             %% "play-ui"                  % "8.8.0-play-26",
     "com.beachape"            %% "enumeratum-play-json"     % "1.5.13",
     "com.luketebbs.uniform"   %% "interpreter-play26"       % uniformVersion,
-    "uk.gov.hmrc"             %% "mongo-caching"            % "6.1.0-play-26",
-    "uk.gov.hmrc"             %% "simple-reactivemongo"     % "7.23.0-play-26",
+    "uk.gov.hmrc"             %% "mongo-caching"            % "6.8.0-play-26",
+    "uk.gov.hmrc"             %% "simple-reactivemongo"     % "7.24.0-play-26",
     "org.reactivemongo"       %% "play2-reactivemongo"      % "0.18.6-play26",
-    "uk.gov.hmrc"             %% "auth-client"              % "2.20.0-play-26",
-    "uk.gov.hmrc"             %% "play-language"            % "3.4.0",
+    "uk.gov.hmrc"             %% "auth-client"              % "2.35.0-play-26",
+    "uk.gov.hmrc"             %% "play-language"            % "4.1.0",
     "commons-validator"       % "commons-validator"         % "1.6",
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.3.0",
-    "uk.gov.hmrc"             %% "play-language"            % "3.4.0"
+    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.5.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.3.0" % Test classifier "tests",
+    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.5.0" % Test classifier "tests",
     "org.scalatest"           %% "scalatest"                % "3.0.8"                 % "test",
     "org.jsoup"               %  "jsoup"                    % "1.10.2"                % "test",
     "com.typesafe.play"       %% "play-test"                % current                 % "test",
