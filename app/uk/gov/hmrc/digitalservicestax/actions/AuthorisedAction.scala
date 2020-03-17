b5d27616d2b1bb0a8fae814d0279843a5faec13f
adamdye-hmrc <31273792+adamdye-hmrc@users.noreply.github.com>
2020-03-17 13:13:33 +0000

    Dst 192 (#52)
    * add messages into authorisedaction
    * Added wrong account type pages.
    
    Co-authored-by: Luke Tebbs <luke.tebbs@digital.hmrc.gov.uk>

--- app/uk/gov/hmrc/digitalservicestax/actions/AuthorisedAction.scala
+++ app/uk/gov/hmrc/digitalservicestax/actions/AuthorisedAction.scala
@@ -16,14 +16,16 @@
 
 package uk.gov.hmrc.digitalservicestaxfrontend.actions
 
+import cats.syntax.semigroup._
 import javax.inject.Inject
 import ltbs.uniform.UniformMessages
 import play.api.Logger
-import play.api.i18n.MessagesApi
+import play.api.i18n.{I18nSupport, Lang, MessagesApi}
 import play.api.mvc.Results.{Continue, Forbidden, Ok, Redirect}
 import play.api.mvc._
 import play.api.http.Status._
-import play.twirl.api.Html
+import play.twirl.api.{Html, HtmlFormat}
+import ltbs.uniform.UniformMessages
 import uk.gov.hmrc.auth.core.AffinityGroup.{Agent, Individual, Organisation}
 import uk.gov.hmrc.auth.core._
 import uk.gov.hmrc.auth.core.AuthProvider.{GovernmentGateway, Verify}
@@ -31,9 +33,10 @@ import uk.gov.hmrc.auth.core.retrieve.v2.Retrievals.{affinityGroup, allEnrolment
 import uk.gov.hmrc.auth.core.retrieve.{Name, ~}
 import uk.gov.hmrc.digitalservicestax.config.AppConfig
 import uk.gov.hmrc.digitalservicestax.connectors.DSTConnector
-import uk.gov.hmrc.digitalservicestax.controllers.{DSTInterpreter, routes}
-import uk.gov.hmrc.digitalservicestax.data.{Registration, InternalId}
+import uk.gov.hmrc.digitalservicestax.controllers.{DSTInterpreter, JourneyController, routes}
+import uk.gov.hmrc.digitalservicestax.data.{InternalId, Registration}
 import uk.gov.hmrc.digitalservicestax.views
+import uk.gov.hmrc.digitalservicestax.views.html.main_template
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.HeaderCarrierConverter
 
@@ -43,12 +46,15 @@ import scala.concurrent.{ExecutionContext, Future}
 class AuthorisedAction @Inject()(
   mcc: MessagesControllerComponents,
   val authConnector: AuthConnector
-)(implicit val appConfig: AppConfig, val executionContext: ExecutionContext)
+)(implicit val appConfig: AppConfig, val executionContext: ExecutionContext, val messagesApi: MessagesApi)
   extends ActionBuilder[AuthorisedRequest, AnyContent] with ActionRefiner[Request, AuthorisedRequest] with AuthorisedFunctions {
 
   override protected def refine[A](request: Request[A]): Future[Either[Result, AuthorisedRequest[A]]] = {
+    import ltbs.uniform.interpreters.playframework.RichPlayMessages
     implicit val req: Request[A] = request
     implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromHeadersAndSession(request.headers, Some(request.session))
+    implicit val msg: UniformMessages[Html] = messagesApi.preferred(request).convertMessagesTwirlHtml(escapeHtml = false) |+|
+      UniformMessages.bestGuess.map(HtmlFormat.escape)
 
     val retrieval =  allEnrolments and credentialRole and internalId and affinityGroup
 
@@ -59,21 +65,30 @@ class AuthorisedAction @Inject()(
         throw new IllegalStateException("Invalid internal ID")
       )
 
+
       Future.successful(Right(AuthorisedRequest(internalId, enrolments, request)))
 
     } recover {
       case ex: UnsupportedCredentialRole =>
+
+
         Logger.warn(
           s"unsupported credential role on account, with message ${ex.msg}, for reason ${ex.reason}",
           ex)
-        Left(Ok(
-          Html("Incorrect account type")
+        Left(Ok(main_template(
+          title =
+            s"${msg("common.title.short")} - ${msg("common.title")}"
+        )(views.html.errors.incorrect_account_affinity()(msg))
+
         ))
       case af: UnsupportedAffinityGroup =>
         Logger.warn(s"invalid account affinity type, with message ${af.msg}, for reason ${af.reason}",
           af)
-        Left(Ok(
-          Html("Incorrect account type")
+        Left(Ok(main_template(
+          title =
+            s"${msg("common.title.short")} - ${msg("common.title")}"
+        )(views.html.errors.incorrect_account_affinity()(msg))
+
         ))
       case _ : NoActiveSession =>
         Logger.info(s"Recover - no active session")
