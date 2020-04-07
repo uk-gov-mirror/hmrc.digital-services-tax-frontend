5e1fa5a59124bbd0e7f13154e396a60833139348
adamdye-hmrc <adam.dye@digital.hmrc.gov.uk>
2020-04-07 11:45:44 +0100

    wip

--- app/uk/gov/hmrc/digitalservicestax/aa_data/package.scala
+++ app/uk/gov/hmrc/digitalservicestax/aa_data/package.scala
@@ -109,11 +109,12 @@ package object data extends SimpleJson {
 
   type PhoneNumber = String @@ PhoneNumber.Tag
   object PhoneNumber extends RegexValidatedString(
-    "^[0-9 ]{6,30}$"
+//    "^[0-9 ]{6,30}$"
+    "^[A-Z0-9)/(\\-*#+]+$"
     // TODO: check phone number regex
 //    """^(?:(?:\(?(?:0(?:0|11)\)?[\s-]?\(?|\+)44\)?[\s-]?(?:\(?0\)?[\s-]?)?)|(?:\(?0))(?:(?:\d{5}\)?[\s-]
 //      |?\d{4,5})|(?:\d{4}\)?[\s-]?(?:\d{5}|\d{3}[\s-]?\d{3}))|(?:\d{3}\)?[\s-]?\d{3}[\s-]?\d{3,4})|
-//      |(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]?(?:x|ext\.?|\#)\d{3,4})?$""".stripMargin.replace("\n","") 
+//      |(?:\d{2}\)?[\s-]?\d{4}[\s-]?\d{4}))(?:[\s-]?(?:x|ext\.?|\#)\d{3,4})?$""".stripMargin.replace("\n","")
 //    _.filter(_.isDigit)
   )
 
