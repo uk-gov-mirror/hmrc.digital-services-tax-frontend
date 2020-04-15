fb2d75b9961d389ba9c742d39e360754a4f3b676
Simon Bailey <5601214+thebailers@users.noreply.github.com>
2020-04-15 16:07:03 +0100

    dst-211 fix timeout issues

--- public/javascripts/timeout-dialog.js
+++ public/javascripts/timeout-dialog.js
@@ -1,8 +1,9 @@
 if(window.location.pathname !== "/digital-services-tax/time-out") {
-    $.timeoutDialog({
+    GOVUK.sessionTimeout({
         timeout: 900,
         countdown: 120,
         keep_alive_url: window.location.href,
-        restart_on_yes: true,
-        logout_url: '/digital-services-tax/time-out'
-    })};
+        logout_url:'/digital-services-tax/time-out',
+        timed_out_url: '/digital-services-tax/time-out'
+    })
+};
