691c24891cb5828a7a26ddaaaa10aecf3d6f4c7d
gavinharris43 <45851646+gavinharris43@users.noreply.github.com>
2020-07-16 13:53:27 +0100

    DST-149 fixed form modified view

--- app/uk/gov/hmrc/digitalservicestax/aa_data/Period.scala
+++ app/uk/gov/hmrc/digitalservicestax/aa_data/Period.scala
@@ -38,21 +38,13 @@ object Period {
     def validateAndTransform(in: String): Option[String] = 
       Some(in).filter{x => x.nonEmpty && x.size <= 4}
   }
-  def apply(
-             start: LocalDate,
-             end: LocalDate,
-             returnDue: LocalDate,
-             key: String): Period = new Period(start, end, returnDue, Period.Key(key))
-
-  def unapply(arg: Period): Option[(LocalDate, LocalDate, LocalDate, Key)] = Option((arg.start, arg.end, arg.returnDue, arg.key))
 
   val periodForm: Form[Period] = Form(
     mapping(
       "start" -> localDate,
       "end" -> localDate,
       "returnDue" -> localDate,
-      "key" -> nonEmptyText
-    )(Period.apply)(Period.unapply)
+      "key" -> nonEmptyText.transform(Period.Key.apply, {x: Period.Key => x.toString})
+    )(apply)(unapply)
   )
-
 }
