12e9107f96c8a51c098ca951039a95af2a6bec1d
Adam Dye <adam.dye@digital.hmrc.gov.uk>
2020-11-23 10:25:53 +0000

    Added regex for check on Eire postcodes.

--- app/uk/gov/hmrc/digitalservicestax/aa_data/package.scala
+++ app/uk/gov/hmrc/digitalservicestax/aa_data/package.scala
@@ -48,10 +48,19 @@ package object data extends SimpleJson {
   )
 
   type Postcode = String @@ Postcode.Tag
-  object Postcode extends RegexValidatedString(
-    """^(GIR 0A{2})|((([A-Z][0-9]{1,2})|(([A-Z][A-HJ-Y][0-9]{1,2})|(([A-Z][0-9][A-Z])|([A-Z][A-HJ-Y][0-9]?[A-Z]))))[ ]?[0-9][A-Z]{2})$""",
-    _.trim.replaceAll("[ \\t]+", " ").toUpperCase
-  )
+  object Postcode extends ValidatedType[String] {
+    private val ukPostcodeRegex = """^(GIR 0A{2})|((([A-Z][0-9]{1,2})|(([A-Z][A-HJ-Y][0-9]{1,2})|(([A-Z][0-9][A-Z])|([A-Z][A-HJ-Y][0-9]?[A-Z]))))[ ]?[0-9][A-Z]{2})$"""
+    private val eirPostcodeRegex = """\b(?:(a(4[125s]|6[37]|7[5s]|[8b][1-6s]|9[12468b])|c1[5s]|d([0o][1-9sb]|1[0-8osb]|2[024o]|6w)|e(2[15s]|3[24]|4[15s]|[5s]3|91)|f(12|2[368b]|3[15s]|4[25s]|[5s][26]|9[1-4])|h(1[2468b]|23|[5s][34]|6[25s]|[79]1)|k(3[246]|4[5s]|[5s]6|67|7[8b])|n(3[79]|[49]1)|p(1[247]|2[45s]|3[126]|4[37]|[5s][16]|6[17]|7[25s]|[8b][15s])|r(14|21|3[25s]|4[25s]|[5s][16]|9[35s])|t(12|23|34|4[5s]|[5s]6)|v(1[45s]|23|3[15s]|42|9[2-5s])|w(12|23|34|91)|x(3[5s]|42|91)|y(14|2[15s]|3[45s]))\s?[abcdefhknoprtsvwxy\d]{4})\b"""
+
+    private def checkPostcode(in: String, regex: String): Boolean = {
+      !in.trim.replaceAll("[ \\t]+", " ").toUpperCase.matches(regex)
+    }
+    override def validateAndTransform(in: String): Option[String] = {
+      Some(in).filter(x =>
+        checkPostcode(x, ukPostcodeRegex) || checkPostcode(x, eirPostcodeRegex)
+      )
+    }
+  }
 
   type Money = BigDecimal @@ Money.Tag
   object Money extends ValidatedType[BigDecimal] {
